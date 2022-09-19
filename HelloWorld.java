9b9bc677b82fa19b68f3d8ec0726c5af0eb05975
Conner Hoy <connernexus9@gmail.com>
2022-09-19 10:41:40 -0400

    homeworkOne

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+//Name : Conner Hoy
+//Class: CSET 3600
+//Instructor: Don Miller
+//Homework: HW1 Hello World
+//Date: 09/19/22
+
+public class HelloWorld {
+public static void main(String[] args) {
+    System.out.print("Hello World");
+    
+}
+}
